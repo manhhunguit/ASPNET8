32236f37ae9043e25a18ea737e1e86c180879526
Hung Nguyen <manhhunguit@gmail.com>
2025-02-16 11:42:17 +0700

    Add XIntegrationTest with simple tests.

---
+++ ASPNET8.XIntegrationTest/Services/UserServiceIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using ASPNET8.Services;
+
+namespace ASPNET8.XIntegrationTest.Services
+{
+    public class UserServiceIntegrationTest
+    {
+        private readonly UserService _userService;
+
+        public UserServiceIntegrationTest()
+        {
+            var roleService = new RoleService();
+            _userService = new UserService(roleService);
+        }
+
+        [Fact]
+        public void GetByRoleId_ShouldReturnUsers()
+        {
+            // Arrange
+            var roleId = 1;
+
+            // Act
+            var users = _userService.GetByRoleId(roleId);
+
+            // Assert
+            Assert.NotNull(users);
+            Assert.True(users.Any());
+        }
+    }
+}
